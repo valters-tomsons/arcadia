feccc7e4b2c14f937fa5ea91fc6168aaae228003
valters-tomsons <valters.tomsons@protonmail.com>
2023-07-02 07:40:18 +0300

    Add listener

--- src/server/Program.cs
+++ src/server/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Security.Cryptography;
+
+var server = new TcpListener(System.Net.IPAddress.Any, 18800);
+server.Start();
+
+var bufferStream = new MemoryStream();
+var buffer = new byte[1024];
+
+while(true)
+{
+    var client = await server.AcceptTcpClientAsync();
+    var stream = client.GetStream();
+
+    while(stream.DataAvailable)
+    {
+        var bytes = await stream.ReadAsync(buffer);
+        await bufferStream.WriteAsync(buffer.AsMemory(0, bytes));
+
+        var dataHash = SHA256.HashData(bufferStream.ToArray().Take(bytes).ToArray());
+        var hash = BitConverter.ToString(dataHash).Replace("-", "").ToLower();
+        Console.WriteLine(hash);
+    }
+
+    bufferStream.Position = 0;
+}
