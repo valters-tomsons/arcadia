55277799b06ef7571c284f42dd994bb802302c1f
valters-tomsons <valters.tomsons@protonmail.com>
2023-07-14 15:29:33 +0300

    Add appsettings.json

--- src/server/AppSettings.cs
+++ src/server/AppSettings.cs
@@ -6,5 +6,5 @@ public record AppSettings
 {
     public int Port { get; init; } = Beach.FeslPort;
     public string UpstreamHost { get; init; } = "beach-ps3.fesl.ea.com";
-    public bool MirrorUpstreamCert { get; init; } = true;
+    public bool MirrorUpstreamCert { get; init; }
 }
