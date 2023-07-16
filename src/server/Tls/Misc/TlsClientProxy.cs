f89ac348627aa2988027fc27b7f55221376a2af1
valters-tomsons <valters.tomsons@protonmail.com>
2023-07-16 23:50:33 +0300

    Move auth into proxy file

--- src/server/Tls/Misc/TlsClientProxy.cs
+++ src/server/Tls/Misc/TlsClientProxy.cs
@@ -96,3 +96,16 @@ public class TlsClientProxy
         }
     }
 }
+
+public class ProxyTlsAuthentication : TlsAuthentication
+{
+    public TlsCredentials GetClientCredentials(CertificateRequest certificateRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void NotifyServerCertificate(TlsServerCertificate serverCertificate)
+    {
+        Console.WriteLine("Ignoring server certificate...");
+    }
+}
