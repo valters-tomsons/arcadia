1fe2246ba1e861f394b038065fa07a41c8d9ffa6
valters-tomsons <valters.tomsons@protonmail.com>
2023-07-21 03:34:28 +0300

    Reorder methods

--- src/server/Fesl/ArcadiaFesl.cs
+++ src/server/Fesl/ArcadiaFesl.cs
@@ -70,77 +70,6 @@ public class ArcadiaFesl
         }
     }
 
-    private async Task HandleAddAccount(FeslPacket request)
-    {
-        var data = new Dictionary<string, object>
-        {
-            {"TXN", "NuPS3AddAccount"},
-        };
-
-        var email = request.DataDict["nuid"] as string;
-        var pass = request.DataDict["password"] as string;
-
-        Console.WriteLine($"Trying to register user {email} with password {pass}");
-
-        var packet = new FeslPacket("acct", 0x80000000, data);
-        var response = await packet.ToPacket(_plasmaTicketId);
-
-        _network.WriteApplicationData(response.AsSpan());
-        Console.WriteLine(Encoding.ASCII.GetString(response));
-    }
-
-    private async Task HandleGetTos()
-    {
-        const string tos = "Welcome to Arcadia!\nBeware, here be dragons!";
-
-        var data = new Dictionary<string, object>
-        {
-            { "TXN", "NuGetTos" },
-            { "version", "20426_17.20426_17" },
-            { "tos", $"{System.Net.WebUtility.UrlEncode(tos).Replace('+', ' ')}" },
-        };
-
-        var packet = new FeslPacket("acct", 0x80000000, data);
-        var response = await packet.ToPacket(_plasmaTicketId);
-
-        _network.WriteApplicationData(response.AsSpan());
-        Console.WriteLine(Encoding.ASCII.GetString(response));
-    }
-
-    private async Task HandleLogin(FeslPacket request)
-    {
-        var encryptedSet = request.DataDict.TryGetValue("returnEncryptedInfo", out var returnEncryptedInfo);
-        Console.WriteLine($"returnEncryptedInfo: {returnEncryptedInfo} ({encryptedSet})");
-
-        var loginResponseData = new Dictionary<string, object>();
-
-        var tosAccepted = request.DataDict.TryGetValue("tosVersion", out var tosAcceptedValue);
-
-        if (!tosAccepted || string.IsNullOrEmpty(tosAcceptedValue as string))
-        {
-            loginResponseData.Add("TXN", "NuPS3Login");
-            loginResponseData.Add( "localizedMessage", "The user was not found" );
-            loginResponseData.Add( "errorContainer.[]", 0 );
-            loginResponseData.Add( "errorCode", 101 );
-        }
-        else
-        {
-            const string keyTempl = "W5NyZzx{0}Cki6GQAAKDw.";
-            var lkey = string.Format(keyTempl, "123456789");
-
-            loginResponseData.Add("lkey", lkey);
-            loginResponseData.Add("TXN", "NuPS3Login");
-            loginResponseData.Add("userId", 1000000000000);
-            loginResponseData.Add("personaName", "faith");
-        }
-
-        var loginPacket = new FeslPacket("acct", 0x80000000, loginResponseData);
-        var loginResponse = await loginPacket.ToPacket(_plasmaTicketId);
-
-        _network.WriteApplicationData(loginResponse.AsSpan());
-        Console.WriteLine(Encoding.ASCII.GetString(loginResponse));
-    }
-
     private async Task HandleHello()
     {
         var currentTime = DateTime.UtcNow.ToString("MMM-dd-yyyy HH:mm:ss 'UTC'", CultureInfo.InvariantCulture);
@@ -176,5 +105,76 @@ public class ArcadiaFesl
 
         _network.WriteApplicationData(memcheckResponse.AsSpan());
         Console.WriteLine(Encoding.ASCII.GetString(memcheckResponse));
+    }
+
+    private async Task HandleGetTos()
+    {
+        const string tos = "Welcome to Arcadia!\nBeware, here be dragons!";
+
+        var data = new Dictionary<string, object>
+        {
+            { "TXN", "NuGetTos" },
+            { "version", "20426_17.20426_17" },
+            { "tos", $"{System.Net.WebUtility.UrlEncode(tos).Replace('+', ' ')}" },
+        };
+
+        var packet = new FeslPacket("acct", 0x80000000, data);
+        var response = await packet.ToPacket(_plasmaTicketId);
+
+        _network.WriteApplicationData(response.AsSpan());
+        Console.WriteLine(Encoding.ASCII.GetString(response));
+    }
+
+    private async Task HandleLogin(FeslPacket request)
+    {
+        var encryptedSet = request.DataDict.TryGetValue("returnEncryptedInfo", out var returnEncryptedInfo);
+        Console.WriteLine($"returnEncryptedInfo: {returnEncryptedInfo} ({encryptedSet})");
+
+        var loginResponseData = new Dictionary<string, object>();
+
+        var tosAccepted = request.DataDict.TryGetValue("tosVersion", out var tosAcceptedValue);
+
+        if (!tosAccepted || string.IsNullOrEmpty(tosAcceptedValue as string))
+        {
+            loginResponseData.Add("TXN", request.Type);
+            loginResponseData.Add( "localizedMessage", "The user was not found" );
+            loginResponseData.Add( "errorContainer.[]", 0 );
+            loginResponseData.Add( "errorCode", 101 );
+        }
+        else
+        {
+            const string keyTempl = "W5NyZzx{0}Cki6GQAAKDw.";
+            var lkey = string.Format(keyTempl, "123456789");
+
+            loginResponseData.Add("lkey", lkey);
+            loginResponseData.Add("TXN", "NuPS3Login");
+            loginResponseData.Add("userId", 1000000000000);
+            loginResponseData.Add("personaName", "faith");
+        }
+
+        var loginPacket = new FeslPacket("acct", 0x80000000, loginResponseData);
+        var loginResponse = await loginPacket.ToPacket(_plasmaTicketId);
+
+        _network.WriteApplicationData(loginResponse.AsSpan());
+        Console.WriteLine(Encoding.ASCII.GetString(loginResponse));
+    }
+
+    private async Task HandleAddAccount(FeslPacket request)
+    {
+        var data = new Dictionary<string, object>
+        {
+            {"TXN", "NuPS3AddAccount"},
+        };
+
+        var email = request.DataDict["nuid"] as string;
+        var pass = request.DataDict["password"] as string;
+
+        Console.WriteLine($"Trying to register user {email} with password {pass}");
+
+        var packet = new FeslPacket("acct", 0x80000000, data);
+        var response = await packet.ToPacket(_plasmaTicketId);
+
+        _network.WriteApplicationData(response.AsSpan());
+        Console.WriteLine(Encoding.ASCII.GetString(response));
     }
 }
