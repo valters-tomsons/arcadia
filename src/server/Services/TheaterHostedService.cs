eae53e25eab5721d614ac0efd7ecd74030d50008
valters-tomsons <valters.tomsons@protonmail.com>
2023-09-08 03:56:14 +0300

    Extract client hello random and match with session key

--- src/server/Services/TheaterHostedService.cs
+++ src/server/Services/TheaterHostedService.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Org.BouncyCastle.Tls;
 
 namespace Arcadia.Services;
 
--- tools/dump-pcap/MainWindow.axaml.cs
+++ tools/dump-pcap/MainWindow.axaml.cs
@@ -1,8 +1,18 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Arcadia.Tls.Crypto;
 using Avalonia.Controls;
 using Avalonia.Platform.Storage;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Tls;
+using Org.BouncyCastle.Tls.Crypto;
+using Org.BouncyCastle.Tls.Crypto.Impl.BC;
 using PacketDotNet;
 using SharpPcap;
 using SharpPcap.LibPcap;
@@ -20,7 +30,37 @@ public partial class MainWindow : Window
     private async void OnOpened(object? sender, EventArgs e)
     {
         using var captureFile = await SelectCaptureFile();
-        Analyze(captureFile);
+        if (captureFile == null) throw new Exception("No capture file selected!");
+
+        Dictionary<string, string> sessionKeys = new();
+        using (var sslKeyFile = await SelectKeyFile())
+        {
+            if (sslKeyFile != null)
+            {
+                sessionKeys = await LoadSessionKeys(sslKeyFile);
+            }
+        }
+
+        Analyze(captureFile, sessionKeys);
+    }
+
+    private async Task<IStorageFile?> SelectKeyFile()
+    {
+        var pickerOptions = new FilePickerOpenOptions
+        {
+            Title = "Select a SSLKEYLOG file",
+            AllowMultiple = false,
+            FileTypeFilter = new FilePickerFileType[] {
+                new("All files") {
+                    Patterns = new [] { "*.*", }
+                }
+            }
+        };
+
+        var topLevel = GetTopLevel(this) ?? throw new Exception("MainWindow.GetTopLevel() failed!");
+        var selection = await topLevel.StorageProvider.OpenFilePickerAsync(pickerOptions);
+
+        return selection?.FirstOrDefault();
     }
 
     public async Task<IStorageFile?> SelectCaptureFile()
@@ -42,24 +82,44 @@ public partial class MainWindow : Window
         return selection?.FirstOrDefault();
     }
 
-    public void Analyze(IStorageFile file)
+    public void Analyze(IStorageFile captureFile, Dictionary<string, string> sessionKeys)
     {
         var config = new DeviceConfiguration();
 
-        using var captureDevice = new CaptureFileReaderDevice(file.Path.LocalPath);
+        using var captureDevice = new CaptureFileReaderDevice(captureFile.Path.LocalPath);
         captureDevice.Open(config);
 
         var packets = 0;
         while (captureDevice.GetNextPacket(out var packet) != GetPacketStatus.NoRemainingPackets)
         {
-            HandleCapturedPacket(packet);
+            HandleCapturedPacket(packet, sessionKeys);
             packets++;
         }
 
         Console.WriteLine($"Total packets: {packets}");
     }
 
-    private void HandleCapturedPacket(PacketCapture e)
+    private async Task<Dictionary<string, string>> LoadSessionKeys(IStorageFile keyFile)
+    {
+        var sessionKeys = new Dictionary<string, string>();
+        var lines = await File.ReadAllLinesAsync(keyFile.Path.LocalPath);
+
+        foreach(var line in lines)
+        {
+            if (!line.StartsWith("CLIENT_RANDOM")) continue;
+
+            var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 3) continue;
+
+            var clientRandom = parts[1];
+            var sessionKey = parts[2];
+            sessionKeys.Add(clientRandom, sessionKey);
+        }
+
+        return sessionKeys;
+    }
+
+    private void HandleCapturedPacket(PacketCapture e, Dictionary<string, string> sessionKeys)
     {
         var capture = e.GetPacket();
         var ethPacket = Packet.ParsePacket(capture.LinkLayerType, capture.Data) as EthernetPacket;
@@ -75,17 +135,38 @@ public partial class MainWindow : Window
         var destPort = transportPacket.DestinationPort;
         Console.WriteLine($"{e.Header.Timeval} | {source}:{sourcePort} -> {dest}:{destPort}");
 
-        if (transportPacket is TcpPacket tcpPacket)
+        if (transportPacket is TcpPacket tcpPacket && IsSslV3Packet(tcpPacket.PayloadData))
         {
-            IdentifyTls(tcpPacket.PayloadData);
+            InitializeCipher(tcpPacket.PayloadData);
         }
     }
 
-    private (byte majorVer, byte minorVer) IdentifyTls(byte[] payload)
+    private void InitializeCipher(byte[] payload, Dictionary<string, string> sessionKeys)
     {
-        if (payload.Length == 0 || payload[0] != 0x16)
+        var clientRandom = Crypto.ExtractClientRandomFromClientHello(payload);
+        if (clientRandom is null) return;
+
+        var clientKey = BitConverter.ToString(clientRandom).Replace("-", "");
+
+        sessionKeys.TryGetValue(clientKey, out var sessionKeyString);
+        if (sessionKeyString is null) return;
+
+        // var crypto = new BcTlsCrypto();
+        // crypto.CreateCipher(new Rc4TlsCrypto());
+
+        // var kk = new KeyParameter()
+    }
+
+    private void Decrypt(byte[] payload, Dictionary<string, string> sessionKeys)
+    {
+        var crypto = new Rc4TlsCrypto();
+    }
+
+    private bool IsSslV3Packet(byte[] payload)
+    {
+        if (payload.Length < 5)
         {
-            return (0,0);
+            return false;
         }
 
         var recordLength = (payload[3] << 8) + payload[4];
@@ -93,17 +174,14 @@ public partial class MainWindow : Window
 
         if (!tlsLengthValid)
         {
-            return (0,0);
+            return false;
         }
 
-        var majorVer = payload[1];
-        var minorVer = payload[2];
-
-        if (majorVer == 3 && minorVer == 0)
+        if (payload[1] != 3 && payload[2] != 0)
         {
-            Console.WriteLine("> SSLv3");
+            return false;
         }
 
-        return (majorVer, minorVer);
+        return true;
     }
 }
