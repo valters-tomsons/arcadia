02652aa8cf0274c12fc05c6df2a7bb7d09768d3f
Valters Tomsons <valters@tomsons.me>
2023-09-24 07:26:57 +0300

    Implement somewhat working RPCN ticket reader

---
+++ src/server/Psn/RpcnTicketDecoder.cs
@@ -0,0 +1,80 @@
+using System.Text;
+
+namespace Arcadia.Psn;
+
+public static class RPCNTicketDecoder
+{
+    public static IEnumerable<TicketData> DecodeFromASCIIString(string asciiString)
+    {
+        var bytes = Convert.FromHexString(asciiString[1..]);
+
+        using var stream = new MemoryStream(bytes);
+        using var reader = new BinaryReader(stream);
+
+        while(reader.BaseStream.Position < reader.BaseStream.Length)
+        {
+            var ticket = ReadTicketData(reader);
+            if (ticket is not null) yield return ticket;
+        }
+    }
+
+
+    private static TicketData? ReadTicketData(BinaryReader reader)
+    {
+        ushort id = BitConverter.ToUInt16(BitConverter.GetBytes(reader.ReadUInt16()).Reverse().ToArray(), 0);
+        ushort len = BitConverter.ToUInt16(BitConverter.GetBytes(reader.ReadUInt16()).Reverse().ToArray(), 0);
+
+        var type = (TicketDataType)(id & 0x0FFF);
+
+        switch (type)
+        {
+            case TicketDataType.Empty:
+                return new EmptyData();
+
+            case TicketDataType.U32:
+                byte[] u32Bytes = reader.ReadBytes(4);
+                Array.Reverse(u32Bytes);
+                return new U32Data { Value = BitConverter.ToUInt32(u32Bytes, 0) };
+
+            case TicketDataType.U64:
+                byte[] u64Bytes = reader.ReadBytes(8);
+                Array.Reverse(u64Bytes);
+                return new U64Data { Value = BitConverter.ToUInt64(u64Bytes, 0) };
+
+            case TicketDataType.Time:
+                byte[] timeBytes = reader.ReadBytes(8);
+                Array.Reverse(timeBytes);
+                return new TimeData { Value = BitConverter.ToUInt64(timeBytes, 0) };
+
+            case TicketDataType.Binary:
+                return new BinaryData { Value = reader.ReadBytes(len) };
+
+            case TicketDataType.BString:
+                var str = Encoding.UTF8.GetString(reader.ReadBytes(len));
+                return new BStringData { Value = str.TrimEnd('\0') };
+
+            case TicketDataType.Blob:
+                var blobData = new BlobData
+                {
+                    Tag = reader.ReadByte(),
+                    Children = new List<TicketData>()
+                };
+
+                ushort remainingLength = (ushort)(len - 1); // Subtract 1 for the tag
+
+                while (remainingLength > 0)
+                {
+                    var child = ReadTicketData(reader);
+                    blobData.Children.Add(child);
+                    remainingLength -= (ushort)(4 + child.Length); // 4 bytes for id and len
+                }
+
+                return blobData;
+
+            default:
+                Console.WriteLine($"Unknown or unhandled type: {id}");
+                reader.BaseStream.Seek(len, SeekOrigin.Current);  // Skip the unknown type
+                return null;
+        }
+    }
+}
--- src/server/Psn/TicketData.cs
+++ src/server/Psn/TicketData.cs
@@ -1,28 +1,60 @@
 namespace Arcadia.Psn;
 
-public enum TicketType
+public enum TicketDataType
 {
-    Empty,
-    U32,
-    U64,
-    BString,
-    Time,
-    Binary,
-    Blob
+    Empty = 0,
+    U32 = 1,
+    U64 = 2,
+    Time = 7,
+    Binary = 8,
+    BString = 4,
+    Blob = 0x3000
 }
 
-public struct TicketData
+public abstract class TicketData
 {
-    public TicketType Type;
-    public ushort Id;
-    public ushort Len;
-    public byte[] Data;
-    public TicketData[] SubData;
+    public abstract TicketDataType Type { get; }
+    public ushort Length { get; set; }
 }
 
-public struct Ticket
+public class EmptyData : TicketData
 {
-    public uint Version;
-    public uint Size;
-    public List<TicketData> Data;
+    public override TicketDataType Type => TicketDataType.Empty;
+}
+
+public class U32Data : TicketData
+{
+    public override TicketDataType Type => TicketDataType.U32;
+    public uint Value { get; set; }
+}
+
+public class U64Data : TicketData
+{
+    public override TicketDataType Type => TicketDataType.U64;
+    public ulong Value { get; set; }
+}
+
+public class TimeData : TicketData
+{
+    public override TicketDataType Type => TicketDataType.Time;
+    public ulong Value { get; set; }
+}
+
+public class BinaryData : TicketData
+{
+    public override TicketDataType Type => TicketDataType.Binary;
+    public byte[] Value { get; set; }
+}
+
+public class BStringData : TicketData
+{
+    public override TicketDataType Type => TicketDataType.BString;
+    public string Value { get; set; }
+}
+
+public class BlobData : TicketData
+{
+    public override TicketDataType Type => TicketDataType.Blob;
+    public byte Tag { get; set; }
+    public List<TicketData> Children { get; set; }
 }
