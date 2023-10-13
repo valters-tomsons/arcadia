e371764c829a6a3a4c7b3891039453b4d3760c46
valters-tomsons <valters.tomsons@protonmail.com>
2023-10-13 15:01:17 +0300

    Add tests for PSN ticket

--- src/tests/PSNTicketTests.cs
+++ src/tests/PSNTicketTests.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using Arcadia.PSN;
+using System.Text;
 
 namespace tests;
 
@@ -13,16 +14,30 @@ public class PSNTicketTests
         var ticketData = TicketDecoder.DecodeFromASCIIString(loginTicket);
         var ticket = new PSNTicket(ticketData!);
 
-        Assert.True(ticket.ServiceId.Equals("EP0006-NPEB00092_00"));
-        Assert.True(ticket.Domain.Equals("un"));
-        Assert.True(ticket.Region.Equals("br"));
+        Assert.Equal("EP0006-NPEB00092_00", ticket.ServiceId);
+        Assert.Equal("un", ticket.Domain);
+        Assert.Equal("br", ticket.Region);
 
-        Assert.True(ticket.OnlineId.Equals("faith"));
+        Assert.Equal("faith", ticket.OnlineId);
 
         Assert.True(ticket.IssuedDate.Year == 2023 && ticket.IssuedDate.Month == 9 && ticket.IssuedDate.Day == 24);
         Assert.True(ticket.IssuedDate.Hour == 3 && ticket.IssuedDate.Minute == 14 && ticket.IssuedDate.Second == 21);
 
         Assert.True(ticket.ExpireDate.Year == 2023 && ticket.ExpireDate.Month == 9 && ticket.ExpireDate.Day == 24);
         Assert.True(ticket.ExpireDate.Hour == 3 && ticket.ExpireDate.Minute == 29 && ticket.ExpireDate.Second == 21);
+    }
+
+    [Fact]
+    public void ValidTicket_PSN_Decode()
+    {
+        const string loginTicket = $"$31000000000000f8300000ac00080014328845fa37db56a958f4aebcbb8e02392d6d92770001000400000100000700080000018b28d4510d000700080000018b2dfaa9600002000858725221887ac2780004002046616974684c560000000000000000000000000000000000000000000000000000080004676200010004000462350000000800184550303030362d4e50454230303039325f303000000000003011000407ce0302000100041900020030100000000000003002004400080004382de58d000800383036021900ead042d18a5a9dcc5dbd15556c9f809175af357246ac8c780219008766662cb2b8e112532feaa707271cd6eaba659220bc528e";
+
+        var ticketData = TicketDecoder.DecodeFromASCIIString(loginTicket);
+
+        var onlineId = (ticketData[5] as BStringData).Value.TrimEnd('\0');
+        var serviceId = Encoding.UTF8.GetString((ticketData[8] as BinaryData).Value).TrimEnd('\0');
+
+        Assert.Equal("FaithLV", onlineId);
+        Assert.Equal("EP0006-NPEB00092_00", serviceId);
     }
 }
