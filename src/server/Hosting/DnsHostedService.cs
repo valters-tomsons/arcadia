97e7f1af604b892686aab62fb9dbb397c4af6c45
valters-tomsons <valters.tomsons@protonmail.com>
2023-12-23 16:50:28 +0200

    Change Services namespace to Hosting

--- src/server/Hosting/DnsHostedService.cs
+++ src/server/Hosting/DnsHostedService.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Arcadia.Services;
+namespace Arcadia.Hosting;
 
 public class DnsHostedService : IHostedService
 {
