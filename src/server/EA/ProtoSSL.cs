2fae6583b932f73def31cd8f1052d032dbfbe392
valters-tomsons <valters.tomsons@protonmail.com>
2024-01-07 18:39:31 +0200

    Cache fesl certitificates in memory

--- src/server/EA/ProtoSSL.cs
+++ src/server/EA/ProtoSSL.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
 using Org.BouncyCastle.Asn1.X509;
 using Org.BouncyCastle.Crypto;
@@ -19,19 +20,37 @@ namespace Arcadia.EA;
 /// </summary>
 public class ProtoSSL
 {
+    private readonly ILogger<ProtoSSL> _logger;
+
+    private readonly ConcurrentDictionary<string, (AsymmetricKeyParameter, Certificate)> _certCache = new();
     private const string Sha1CipherAlgorithm = "SHA1WITHRSA";
     private static readonly ReadOnlyMemory<byte> Sha1CipherSignature = new byte[] { 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01, 0x05 };
 
-    private readonly ILogger<ProtoSSL> _logger;
     public ProtoSSL(ILogger<ProtoSSL> logger)
     {
         _logger = logger;
     }
 
+    public (AsymmetricKeyParameter, Certificate) GetFeslEaCert()
+    {
+        const string IssuerDN = "CN=OTG3 Certificate Authority, C=US, ST=California, L=Redwood City, O=\"Electronic Arts, Inc.\", OU=Online Technology Group, emailAddress=dirtysock-contact@ea.com";
+        const string SubjectDN = "C=US, ST=California, O=\"Electronic Arts, Inc.\", OU=Online Technology Group, CN=fesl.ea.com, emailAddress=fesl@ea.com";
+        const string cacheKey = "fesl.ea.com";
+
+        if (_certCache.TryGetValue(cacheKey, out var cacheHit))
+        {
+            return cacheHit;
+        }
+
+        var creds = GenerateVulnerableCert(IssuerDN, SubjectDN);
+        _certCache.TryAdd(cacheKey, creds);
+        return creds;
+    }
+
     /// <summary>
     /// Generates a certificate for vulnerable ProtoSSL versions.
     /// </summary>
-    public (AsymmetricKeyParameter, Certificate) GenerateVulnerableCert(string issuer, string subject)
+    private (AsymmetricKeyParameter, Certificate) GenerateVulnerableCert(string issuer, string subject)
     {
         var crypto = new BcTlsCrypto(new SecureRandom());
         var rsaKeyPairGen = new RsaKeyPairGenerator();
