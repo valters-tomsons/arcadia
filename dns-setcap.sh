9b542ae435c3f9fd798c0b6e8fe17158f3ba4833
valters-tomsons <valters.tomsons@protonmail.com>
2024-01-16 01:04:36 +0200

    Update dns-setcap for net8

--- dns-setcap.sh
+++ dns-setcap.sh
@@ -4,5 +4,5 @@ dotnet build
 
 cd src/server
 
-sudo setcap 'cap_net_bind_service=+ep' bin/Debug/net7.0/Arcadia
-./bin/Debug/net7.0/Arcadia
+sudo setcap 'cap_net_bind_service=+ep' bin/Debug/net8.0/Arcadia
+./bin/Debug/net8.0/Arcadia
