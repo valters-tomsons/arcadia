9f33ceee5aa8065e9dc89b2013ec3fc889b8c514
Valters Tomsons <valters.tomsons@protonmail.com>
2024-07-03 13:55:52 +0300

    Remove DNS unprivileged port, add frostbite host ports

--- run-privileged.sh
+++ run-privileged.sh
@@ -4,7 +4,8 @@
 dotnet build
 
 sudo sysctl net.ipv4.ip_unprivileged_port_start=80
-sudo sysctl net.ipv4.ip_unprivileged_port_start=53
+sudo sysctl net.ipv4.ip_unprivileged_port_start=1003
+sudo sysctl net.ipv4.ip_unprivileged_port_start=1001
 
 cd src/server/bin/Debug/net8.0/
 .Arcadia
