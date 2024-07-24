0ea3fa496b16925ea6ab250f04fdb160b240683d
Valters Tomsons <valters.tomsons@protonmail.com>
2024-07-25 00:17:07 +0300

    Rename script to allow_ports

--- allow_ports.sh
+++ allow_ports.sh
@@ -1,11 +1,5 @@
 #/bin/sh
 
-
-dotnet build
-
 sudo sysctl net.ipv4.ip_unprivileged_port_start=80
 sudo sysctl net.ipv4.ip_unprivileged_port_start=1003
 sudo sysctl net.ipv4.ip_unprivileged_port_start=1001
-
-cd src/server/bin/Debug/net8.0/
-.Arcadia
