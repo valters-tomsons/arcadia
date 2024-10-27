0100b64fa9bbc79a1c59073f3f322bdca234dd3d
Valters Tomsons <valters@tomsons.me>
2024-10-27 20:56:41 +0000

    Add Battlefield 1943 to default listening ports

--- src/server/AppSettings.cs
+++ src/server/AppSettings.cs
@@ -6,7 +6,12 @@ public record ArcadiaSettings
 {
     public string ListenAddress { get; init; } = System.Net.IPAddress.Loopback.ToString();
     public string TheaterAddress { get; init; } = "theater.ps3.arcadia";
-    public int[] ListenPorts { get; init; } = [(int)FeslGamePort.RomePS3, (int)TheaterGamePort.RomePS3];
+    public int[] ListenPorts { get; init; } = [
+        (int)TheaterGamePort.RomePS3, 
+        (int)FeslGamePort.RomePS3, 
+        (int)FeslGamePort.BeachPS3
+    ];
+
     public int MessengerPort { get; init; } = 0;
 }
 
