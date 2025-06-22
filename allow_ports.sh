ccb648557f7b0f1ce345f7ea1743bd92a152e676
Valters Tomsons <valters.tomsons@protonmail.com>
2025-06-22 15:39:16 +0300

    Remove redundant entries, setcap +ep

--- allow_ports.sh
+++ allow_ports.sh
@@ -1,5 +1,4 @@
 #/bin/sh
 
 sudo sysctl net.ipv4.ip_unprivileged_port_start=80
-sudo sysctl net.ipv4.ip_unprivileged_port_start=1003
-sudo sysctl net.ipv4.ip_unprivileged_port_start=1001
+sudo setcap 'cap_net_bind_service=+ep' src/server/bin/Debug/net9.0/Arcadia
