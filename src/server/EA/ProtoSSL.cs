5fe30e0d24d3f7a5517109121a52ab08aa48bde2
Valters Tomsons <valters@tomsons.me>
2025-12-14 00:20:17 +0200

    Use real fesl certificate with factorized keys

--- src/server/EA/ProtoSSL.cs
+++ src/server/EA/ProtoSSL.cs
@@ -1,113 +1,50 @@
-using System.Collections.Concurrent;
-using Microsoft.Extensions.Logging;
-using Org.BouncyCastle.Asn1.X509;
+using System.Reflection;
+using Arcadia.Tls.Crypto;
 using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Crypto.Generators;
-using Org.BouncyCastle.Crypto.Operators;
+using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Math;
-using Org.BouncyCastle.Pkcs;
-using Org.BouncyCastle.Security;
 using Org.BouncyCastle.Tls;
-using Org.BouncyCastle.Tls.Crypto;
 using Org.BouncyCastle.Tls.Crypto.Impl.BC;
-using Org.BouncyCastle.Utilities;
 using Org.BouncyCastle.X509;
 
 namespace Arcadia.EA;
 
-/// <summary>
-/// Based on the following article: https://github.com/Aim4kill/Bug_OldProtoSSL
-/// </summary>
-public class ProtoSSL(ILogger<ProtoSSL> logger)
+public class ProtoSSL
 {
-    private readonly ILogger<ProtoSSL> _logger = logger;
+    private readonly Certificate _cert;
+    private readonly AsymmetricKeyParameter _keyParam;
 
-    private readonly ConcurrentDictionary<string, (AsymmetricKeyParameter, Certificate)> _certCache = new();
-    private const string Sha1CipherAlgorithm = "SHA1WITHRSA";
-    private static readonly ReadOnlyMemory<byte> Sha1CipherSignature = new byte[] { 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01, 0x05 };
+    public ProtoSSL(Rc4TlsCrypto crypto)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        using var resource = assembly.GetManifestResourceStream("Arcadia.fesl-pub.der");
+
+        var cert = new X509CertificateParser().ReadCertificate(resource);
+        var tlsCert = new BcTlsCertificate(crypto, cert.GetEncoded());
+
+        _cert = new Certificate([tlsCert]);
+        _keyParam = ConstructRsaPrivateKey();
+    }
 
     public (AsymmetricKeyParameter, Certificate) GetFeslEaCert()
     {
-        const string IssuerDN = "CN=OTG3 Certificate Authority, C=US, ST=California, L=Redwood City, O=\"Electronic Arts, Inc.\", OU=Online Technology Group, emailAddress=dirtysock-contact@ea.com";
-        const string SubjectDN = "C=US, ST=California, O=\"Electronic Arts, Inc.\", OU=Online Technology Group, CN=fesl.ea.com, emailAddress=fesl@ea.com";
-        const string cacheKey = "fesl.ea.com";
-
-        if (_certCache.TryGetValue(cacheKey, out var cacheHit))
-        {
-            // !TODO: New connections will break after running 10 years without a restart 
-            return cacheHit;
-        }
-
-        var creds = GenerateVulnerableCert(IssuerDN, SubjectDN);
-        _certCache.TryAdd(cacheKey, creds);
-        return creds;
+        return (_keyParam, _cert);
     }
 
-    /// <summary>
-    /// Generates a certificate for vulnerable ProtoSSL versions.
-    /// </summary>
-    private (AsymmetricKeyParameter, Certificate) GenerateVulnerableCert(string issuer, string subject)
+    private static AsymmetricKeyParameter ConstructRsaPrivateKey()
     {
-        var crypto = new BcTlsCrypto(new SecureRandom());
-        var rsaKeyPairGen = new RsaKeyPairGenerator();
-        rsaKeyPairGen.Init(new KeyGenerationParameters(crypto.SecureRandom, 1024));
+        BigInteger p = new("111453461317074268353761995724716395361446805418267262156522133799013175060193");
+        BigInteger q = new("114726748596358283670400355329452217110158824439119423212154846632671665145039");
+        BigInteger e = new("3");
 
-        var caKeyPair = rsaKeyPairGen.GenerateKeyPair();
-        var caCertificate = GenerateCertificate(issuer, caKeyPair, caKeyPair.Private);
+        BigInteger n = p.Multiply(q);
+        BigInteger phi = p.Subtract(BigInteger.One).Multiply(q.Subtract(BigInteger.One));
+        BigInteger d = e.ModInverse(phi);
 
-        var cKeyPair = rsaKeyPairGen.GenerateKeyPair();
-        var cCertificate = GenerateCertificate(subject, cKeyPair, caKeyPair.Private, caCertificate);
-        var patched_cCertificate = PatchCertificateSignaturePattern(cCertificate);
+        BigInteger dP = d.Remainder(p.Subtract(BigInteger.One));
+        BigInteger dQ = d.Remainder(q.Subtract(BigInteger.One));
+        BigInteger qInv = q.ModInverse(p);
 
-        var store = new Pkcs12StoreBuilder().Build();
-        var certEntry = new X509CertificateEntry(patched_cCertificate);
-
-        var certDomain = subject.Split("CN=")[1].Split(",")[0];
-
-        _logger.LogDebug("Certificate generated for: {domain}", certDomain);
-
-        store.SetCertificateEntry(certDomain, certEntry);
-        store.SetKeyEntry(certDomain, new AsymmetricKeyEntry(cKeyPair.Private), [certEntry]);
-
-        var chain = new TlsCertificate[] { new BcTlsCertificate(crypto, certEntry.Certificate.GetEncoded()) };
-        var finalCertificate = new Certificate(chain);
-
-        return (cKeyPair.Private, finalCertificate);
-    }
-
-    private static X509Certificate GenerateCertificate(string subjectName, AsymmetricCipherKeyPair subjectKeyPair, AsymmetricKeyParameter issuerPrivKey, X509Certificate? issuerCert = null)
-    {
-        var issuerDn = issuerCert == null ? new X509Name(subjectName) : issuerCert.SubjectDN;
-
-        var certGen = new X509V3CertificateGenerator();
-        var serialNumber = BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(long.MaxValue), new SecureRandom());
-        certGen.SetSerialNumber(serialNumber);
-        certGen.SetIssuerDN(issuerDn);
-        certGen.SetNotBefore(DateTime.UtcNow.Date);
-        certGen.SetNotAfter(DateTime.UtcNow.Date.AddYears(10));
-        certGen.SetSubjectDN(new X509Name(subjectName));
-        certGen.SetPublicKey(subjectKeyPair.Public);
-        var signatureFactory = new Asn1SignatureFactory(Sha1CipherAlgorithm, issuerPrivKey);
-        return certGen.Generate(signatureFactory);
-    }
-
-    private static X509Certificate PatchCertificateSignaturePattern(X509Certificate cCertificate)
-    {
-        var cert = DotNetUtilities.ToX509Certificate(cCertificate);
-        var certDer = cert.GetRawCertData();
-
-        // There must be two signatures in the DER encoded certificate
-        var signature1Offset = Utils.FindBytePattern(certDer, Sha1CipherSignature.Span);
-        var signature2Offset = Utils.FindBytePattern(certDer, Sha1CipherSignature.Span, signature1Offset + Sha1CipherSignature.Length);
-
-        if (signature1Offset == -1 || signature2Offset == -1)
-        {
-            throw new Exception("Failed to find valid signature for patching!");
-        }
-
-        // Patch the second signature to TLS_NULL_WITH_NULL_NULL
-        certDer[signature2Offset + 8] = 0x01;
-
-        return new X509Certificate(certDer);
+        return new RsaPrivateCrtKeyParameters(n, e, d, p, q, dP, dQ, qInv);
     }
 }
