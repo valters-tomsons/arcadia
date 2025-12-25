e675aabcedea4a9b22697593413020c049c59603
Valters Tomsons <valters.tomsons@protonmail.com>
2025-12-25 04:15:58 +0200

    [discord] Moderate for stupid questions

--- src/server/Discord/ModerationService.cs
+++ src/server/Discord/ModerationService.cs
@@ -14,6 +14,38 @@ public class ModerationService(ILogger<ModerationService> logger)
         Language.Russian
     ).Build();
 
+    private readonly string[] stupidPhrases =
+    [
+        "what do I need to do to play online",
+        "how to play online",
+        "how do i play online",
+        "how can i play online",
+        "can we play online",
+
+        "does it work",
+        "does this work",
+        "does online work",
+        "does multiplayer work",
+        "does coop work",
+        "does co-op work",
+        "does pvp work",
+
+        "is it playable",
+        "is online playable",
+        "is multiplayer playable",
+        "is pvp playable",
+        "is coop playable",
+        "playable online",
+        "playable multiplayer",
+
+        "can i play",
+        "can we play",
+        "are we able to play",
+
+        "which modes work",
+        "tutorial only?",
+    ];
+
     public async Task OnMessageReceived(SocketUserMessage msg)
     {
         if (msg.Author.IsBot) return;
@@ -23,14 +55,43 @@ public class ModerationService(ILogger<ModerationService> logger)
 
         var content = msg.Content.Trim();
         if (string.IsNullOrWhiteSpace(content) || content.Length < 8) return;
-
         var lang = _detector.DetectLanguageOf(content);
-        if (lang == Language.English || lang == Language.Unknown) return;
+
+        if (lang != Language.English && lang != Language.Unknown)
+        {
+            await DeleteNonEnglish(msg);
+            return;
+        }
+
+        foreach (var phrase in stupidPhrases)
+        {
+            if (content.Contains(phrase, StringComparison.InvariantCultureIgnoreCase))
+            {
+                await DeleteIlliterate(msg);
+                return;
+            }
+        }
+    }
+
+    private static async Task DeleteNonEnglish(SocketUserMessage msg)
+    {
+        const ulong nonEnglishChannelId = 1450610182995316969;
 
         try
         {
-            const ulong nonEnglishChannelId = 1450610182995316969;
             await msg.ReplyAsync($"Read Rule #4, keep it english outside of <#{nonEnglishChannelId}>");
+            await msg.DeleteAsync();
+        }
+        catch { }
+    }
+
+    private static async Task DeleteIlliterate(SocketUserMessage msg)
+    {
+        const ulong infoChannelId = 1256693500901331044;
+
+        try
+        {
+            await msg.ReplyAsync($"Read <#{infoChannelId}> in its entirety, it's already explained!");
             await msg.DeleteAsync();
         }
         catch { }
