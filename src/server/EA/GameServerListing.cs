b79353e2076482b2eb32e986ea289616dcb7c05d
Valters Tomsons <valters@tomsons.me>
2025-12-26 17:55:34 +0200

    Seperate session/server into files

--- src/server/EA/GameServerListing.cs
+++ src/server/EA/GameServerListing.cs
@@ -2,23 +2,6 @@ using System.Collections.Concurrent;
 
 namespace Arcadia.EA;
 
-public class PlasmaSession
-{
-    public IEAConnection? FeslConnection { get; set; }
-    public IEAConnection? TheaterConnection { get; set; }
-
-    public int PID { get; set; }
-
-    public required long UID { get; init; }
-    public required string NAME { get; init; }
-    public required string LKEY { get; init; }
-
-    public required string ClientString { get; init; }
-    public required string PartitionId { get; init; }
-
-    public required string OnlinePlatformId { get; set; }
-}
-
 public class GameServerListing
 {
     public IEAConnection? TheaterConnection { get; init; }
---
+++ src/server/EA/PlasmaSession.cs
@@ -0,0 +1,18 @@
+namespace Arcadia.EA;
+
+public class PlasmaSession
+{
+    public IEAConnection? FeslConnection { get; set; }
+    public IEAConnection? TheaterConnection { get; set; }
+
+    public int PID { get; set; }
+
+    public required long UID { get; init; }
+    public required string NAME { get; init; }
+    public required string LKEY { get; init; }
+
+    public required string ClientString { get; init; }
+    public required string PartitionId { get; init; }
+
+    public required string OnlinePlatformId { get; set; }
+}
